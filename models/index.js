4bb7254c79f7cdb8a4b494171d3fe5abb1e6ada0
Gavin M <gavinmumpower96@gmail.com>
2022-05-31 22:55:54 -0400

    touch ups on typos

--- models/index.js
+++ models/index.js
@@ -4,17 +4,17 @@ const sequelize = require('../config/connection');
 const Comment = require('./Comment');
 const Post = require('./Post');
 const User = require('./User');
-const Tag = require('./TagPost');
+const Tag = require('./Tag');
 const Favorite = require('./Favorite');
 const TagPost = require('./TagPost');
 
-User.belongsToMany(Post, {
-    as: 'favorites',
+User.belongsToMany(Post, { //putting this many-to-many rel before the one-to-many rel through author_id appears to prioritize the one-to-many rel on queries
+    as: 'Favorite',
     through: Favorite
 });
 
 Post.belongsToMany(User, {
-    as: 'favoriters',
+    as: 'Favorite',
     through: Favorite
 });
 
