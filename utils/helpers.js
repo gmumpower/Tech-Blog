4b642b1d0b04cb394a84d36e74e7b16e9f86e360
Gavin M <gavinmumpower96@gmail.com>
2022-05-31 21:09:54 -0400

    added seeds

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,36 +1,14 @@
 module.exports = {
   format_date: (date) => {
-    // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
-  },
-  format_amount: (amount) => {
-    // format large numbers with commas
-    return parseInt(amount).toLocaleString();
+      // Format date as MM/DD/YYYY
+      return date.toLocaleDateString();
   },
 
-  ifEquals: (arg1, arg2) => {
-    console.log(arg1);
-    console.log(arg2);
-    return arg1 === arg2;
+  isFav: (postID, favsArr=[]) => {
+    return favsArr.map(post => post.id).includes(postID)? 'true': 'false';
   },
 
-  if_contains: (val, string) => {
-    if (!string){
-      return 'false';
-    }
-    arr = string.split(',').map(Number);
-    
-    if (arr.includes(val)){
-      return 'true';
-    }
-
-    return 'false';
-  },
-
-  extract_tagNames: (tags) => {
-    if (!tags){
-      return "";
-    }
-    return tags.map(tag => ` ${tag.tag_name}`);
+  eq: (val1, val2) => {
+    return val1 == val2;
   }
-};
+}
